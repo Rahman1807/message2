bbd76e0e6573c466cfd3433a307662d8daeddff5
Rahman1807 <zia9848@gmail.com>
2025-04-29 11:18:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Thank You!!!🤗";
+  question.innerHTML = "Batane ke liye shukriya🤗";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
